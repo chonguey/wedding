3933070212861f9ba03e91f07970f5e608478302
Landon Shumway <lshumway@proofpoint.com>
2017-02-09 21:49:01 -0700

    Cleanup

--- email-dump-hidden.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once '/home/content/c/h/o/chonguey/html/mercedes-solari/lib/dbCredentials.php';
-	require_once '/home/content/c/h/o/chonguey/html/mercedes-solari/lib/dbLibrary.php';
-	echo _db_GetAllEmail($db);
-?>
-
-
